cb26c19aae15943030e809696edad997f7557e0f
ray-mov <monovray@gmail.com>
2025-03-23 10:39:29 +0530

    startscreen page added

--- app/screens/startups/StartScreen.tsx
+++ app/screens/startups/StartScreen.tsx
@@ -1,17 +1,65 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Text, TouchableOpacity } from "react-native";
 
 function StartScreen() {
-  return <View></View>;
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.title}>Start</Text>
+        <Text style={styles.title}>Your</Text>
+        <Text style={styles.title}>Rides</Text>
+      </View>
+      <View>
+        <TouchableOpacity style={styles.signInButton}>
+          <Text style={styles.signInText}>Sign In</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text style={styles.signUpText}>
+            Don’t have an account?{" "}
+            <Text style={styles.signUpLink}>Sign Up</Text>
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 }
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
-    alignItems: "center",
+    // alignItems: "center",
     paddingHorizontal: 20,
+    gap: 60,
     backgroundColor: "#F9F9F9",
+  },
+  title: {
+    fontSize: 44,
+    fontWeight: "bold",
+    color: "#000",
+    letterSpacing: 4,
+  },
+  signInButton: {
+    width: "100%",
+    height: 50,
+    backgroundColor: "#000",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 8,
+    marginBottom: 20,
+  },
+  signInText: {
+    color: "#FFF",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  signUpText: {
+    fontSize: 14,
+    color: "#666",
+  },
+  signUpLink: {
+    color: "#6200EE",
+    fontWeight: "bold",
   },
 });
 
